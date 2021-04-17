691351b43cd4a504c5c1db967d1a51481358ae6a
AndBondStyle <andbondstyle@gmail.com>
2021-04-17 11:33:01 +0300

    Implement stream inference

--- wavenet/model.py
+++ wavenet/model.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple, List
+from typing import NamedTuple, List, Tuple
 import torch.nn as nn
 import torch
 
@@ -178,3 +178,84 @@ class WaveNetKWS(nn.Module):
         out = self.output_convs(skip)
         out = self.output_dense(out)
         return out
+
+    def lazy_conv(self, x, conv, buffer, dilation):
+        """
+        Lazy convolution call, computing only one sample (axis=1 column), based on one
+        new sample and history buffer. Returns new sample and updated buffer.
+        :param x: single input sample, shape = (1, <conv in channels>, 1)
+        :param conv: 1D convolution layer / function
+        :param buffer: input history buffer, at least (dilation * (kernel_size - 1) + 1)
+        samples in length (this length is required to apply convolution).
+        :param dilation: convolution dilation factor
+        :return: (new convolution output, new history buffer)
+        """
+        assert x.size(2) == 1
+        buffer = torch.cat((buffer[:, :, 1:], x), dim=2)
+        window_size = dilation * (self.kernel_size - 1) + 1
+        out = conv(buffer[:, :, -window_size:])
+        assert out.size(2) == 1
+        return out, buffer
+
+    def init_buffers(self):
+        """
+        Initializes history buffers for stream inference (see below)
+        :return: dict of buffers (just pass them into stream_forward)
+        """
+        shapes = dict(out=(1, self.out_classes, self.input_size))
+        for i, block in enumerate(self.blocks):
+            for sub in ("gate", "filter"):
+                shape = (1, self.residual_channels, block.meta.input_size)
+                shapes[f"block-{i}-{sub}"] = shape
+        # TODO: Looks a bit funky, could be broken by quantization
+        device = self.input_conv.weight.device
+        dtype = self.input_conv.weight.dtype
+        buffers = {
+            key: torch.zeros(shape, dtype=dtype, device=device)
+            for key, shape in shapes.items()
+        }
+        return buffers
+
+    def stream_forward(
+        self, x: torch.Tensor, buffers: dict
+    ) -> Tuple[torch.Tensor, dict]:
+        """
+        Stream inference using cached dilated buffers
+        :param x: one input feature tensor, shape: (1, model.input_channels, 1)
+        :param buffers: buffers dict, can be generated with model.init_buffers method
+        :return: (model output: shape: (1, model.out_classes), new buffers dict)
+        """
+        assert x.size(0) == x.size(2) == 1
+        # Just copying dict items, no tensors involved
+        buffers = buffers.copy()
+        skip_shape = (1, self.skip_channels, 1)
+        skip = torch.zeros(skip_shape, dtype=x.dtype, device=x.device)
+        x = self.input_conv(x)
+
+        for i, block in enumerate(self.blocks):
+            assert x.size(2) == 1
+            residual = x
+
+            dilated_outs = {}
+            for sub in ("gate", "filter"):
+                key = f"block-{i}-{sub}"
+                dilated_outs[sub], buffers[key] = self.lazy_conv(
+                    x, block[f"{sub}_conv"], buffers[key], block.meta.dilation
+                )
+
+            filter_out = torch.tanh(dilated_outs["filter"])
+            gate_out = torch.sigmoid(dilated_outs["gate"])
+            x = filter_out * gate_out
+            x_skip = block["skip_conv"](x)
+            skip += x_skip
+            x = block["residual_conv"](x)
+            x = residual + x
+            x = block["batch_norm"](x)
+            assert x.size(2) == 1
+
+        out = self.output_convs(skip)
+        buffers["out"] = torch.cat((buffers["out"][:, :, 1:], out), dim=2)
+        buffers["out"][:, :, 0] = 0
+        assert buffers["out"].size(2) == self.input_size
+        out = self.output_dense(buffers["out"])
+        return out, buffers
